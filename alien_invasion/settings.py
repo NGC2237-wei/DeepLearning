1b00c86a8b4a91f76c413852acbac233c3253fab
wei.h <734540980@qq.com>
2021-07-12 15:47:18 +0800

    'alien_invasion外星人移动'

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,6 +8,10 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移，为-1表示向左移
+        self.fleet_direction = 1
 
         # 子弹设置
         self.bullet_speed_factor = 1
